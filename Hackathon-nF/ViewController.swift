5c1e61dc2c56375f93dccb03ed1bd89f140ac810
Nazym-MU <nazym@uni.minerva.edu>
2024-05-25 10:30:12 +0500

    Initial Commit

---
+++ Hackathon-nF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackathon-nF
+//
+//  Created by Назым Жиенгалиева on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
